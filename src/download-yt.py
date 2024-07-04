1ecabbd29e5e6bbcd012ca6631ee9397a9bec504
edgarmejiav <legamejiav@gmail.com>
2024-07-04 15:40:47 -0500

    feat: to yt_dlp

--- src/download-yt.py
+++ src/download-yt.py
@@ -1,33 +1,20 @@
 #!/usr/bin/env python
-from pytube import YouTube
+import yt_dlp
 
-def download_video(url, output_path='./'):
-    print("1")
-    try:
-        video = YouTube(url)
-        print("2")
-        stream = video.streams.get_highest_resolution()
-        video_info = {
-            "title": video.title,
-            "author": video.author,
-            "filename": stream.default_filename,
-            "destination_path": output_path
-        }
-        print("3")
-        stream.download(output_path=output_path)
-        print("Video descargado con éxito!" + video_info["filename"])
-        return video_info
-    except Exception as e:
-        return {"error": str(e)}
+def download_video(url, output_path="."):
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-def say_hello(name):
-    print(f"Hola {name}!")
+
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 2:
         print("Uso: ./downloader-yt <nombre>")
         sys.exit(1)
-    name = sys.argv[1]
-    say_hello(name)
-    # download_video(url="https://youtu.be/3AtDnEC4zak?list=RD8PTDv_szmL0")
+    url = sys.argv[1]
+    download_video(url)
